18e4313f2c5f7b018e631464ba34d5d506690b3e
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-07-12 18:36:57 +0200

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,18 +1,8 @@
-import {
-  // getCustomBikesPages,
-  // getQuizPages,
-  getBlogPages,
-  getProjectsPages,
-} from '@/app/source';
+import { getBlogPages, getProjectsPages } from '@/app/source';
 import { createSearchAPI } from 'fumadocs-core/search/server';
 
 // Combine all pages from different sources
-const allPages = [
-  // ...getCustomBikesPages(),
-  // ...getQuizPages(),
-  ...getProjectsPages(),
-  ...getBlogPages(),
-];
+const allPages = [...getProjectsPages(), ...getBlogPages()];
 const searchIndexes = allPages.map((page) => ({
   title: page.data.title,
   structuredData: page.data.exports.structuredData || null,
